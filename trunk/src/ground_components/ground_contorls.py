313b72372b388d0b72a807ec885fc402f34ddd39
mrberman87 <mrberman87@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-04-20 23:39:13 +0000

    started making controls for the ground station to get data

---
+++ trunk/src/ground_components/ground_contorls.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Each of these are going to kill the receiver, run the transmitter,
+then restart the reciever, and return the new reciever's pid.
+"""
+
+class ground_controls:
+	def get_pic(self, freq, mod_sch, rx_pid):
+		
+	
+	def get_fft(self, freq, mod_sch, rx_pid):
+		
+	
+	def get_sensors(self, freq, mod_sch, rx_pid):
+		
+	
+	def set_vars(self, freq, mod_sch, rx_pid, new_freq, new_mod_sch, new_timeout):
+		
+	
+	def pid_exists(self, pid):
+		
+	
+	def kill_pid(self, pid):
+		
