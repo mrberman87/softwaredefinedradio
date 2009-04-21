c0b837ed5d98122e5bdee6c636cfa0d3d22371a8
mrberman87 <mrberman87@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-04-21 18:42:49 +0000

    Changed RX module name

--- trunk/src/Rx_Path/dbpsk_Rx_path_UAV.py
+++ trunk/src/Rx_Path/dbpsk_Rx_path_UAV.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 ##################################################
 # Gnuradio Python Flow Graph
-# Title: Rx_path_UAV
+# Title: dbpsk_Rx_path_UAV
 # Author: UAV Group
 # Description: gnuradio flow graph
-# Generated: Tue Apr 21 09:15:43 2009
+# Generated: Tue Apr 21 11:41:18 2009
 ##################################################
 
 from gnuradio import blks2
@@ -12,10 +12,10 @@ from gnuradio import gr
 from grc_gnuradio import blks2 as grc_blks2
 from grc_gnuradio import usrp as grc_usrp
 
-class Rx_path_UAV(gr.top_block):
+class dbpsk_Rx_path_UAV(gr.top_block):
 
 	def __init__(self):
-		gr.top_block.__init__(self, "Rx_path_UAV")
+		gr.top_block.__init__(self, "dbpsk_Rx_path_UAV")
 
 		##################################################
 		# Variables
@@ -35,11 +35,10 @@ class Rx_path_UAV(gr.top_block):
 			omega_relative_limit=0.005,
 			gray_code=True,
 		)
-		self.blks2_packet_decoder_0 = grc_blks2.packet_demod_b(grc_blks2.packet_decoder(
-				access_code="",
-				threshold=-1,
-				callback=lambda ok, payload: self.blks2_packet_decoder_0.recv_pkt(ok, payload),
-			),
+		self.blks2_packet_decoder_0 = grc_blks2.packet_decoder(
+			item_size_out=gr.sizeof_char*1,
+			access_code="",
+			threshold=-1,
 		)
 		self.gr_file_sink_0 = gr.file_sink(gr.sizeof_char*1, "insert file name here")
 		self.gr_throttle_0 = gr.throttle(gr.sizeof_gr_complex*1, (8*samp_rate))
@@ -64,7 +63,7 @@ class Rx_path_UAV(gr.top_block):
 		self.usrp_simple_source_x_0.set_frequency((440e6 + self.freq_offset))
 
 if __name__ == '__main__':
-	tb = Rx_path_UAV()
+	tb = dbpsk_Rx_path_UAV()
 	tb.start()
 	raw_input('Press Enter to quit: ')
 	tb.stop()
