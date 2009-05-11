ae2a9838d7cb5721ed4461272dc91d88061813c7
mrberman87 <mrberman87@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-05-11 20:32:47 +0000

    updated get_batt and get_temp

--- src/uav_components/get_temp.py
+++ src/uav_components/get_temp.py
@@ -4,24 +4,22 @@ import timeit, time, serial
 
 def test():
 	s = serial.Serial(0)
+	push_low(s)
 	#loop while control line is high
 	while(s.getCTS()):
 		pass
 	s.close()
 
-def push_low():
+def push_low(s):
 	#put output line low momentarily
-	s = serial.Serial(0)
 	s.setRTS(1)
 	time.sleep(.001)
 	s.setRTS(0)
-	s.close()
 
 def decode_data(value):
 	#...
 
 if __name__=='__main__':
-	push_low()
 	t=timeit.Timer('test()', "from __main__ import test")
 	fd = open('temp.dat', "w")
 	fd.write(decode_data(100*t.timeit(number=10)))
