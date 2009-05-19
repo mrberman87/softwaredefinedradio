8bd5fe894b9f99c48c312074bff0b29f35717798
c.judah.75 <c.judah.75@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-05-19 23:57:57 +0000

    Tx Path added Variable Chooser to stop Transmit

--- src/Tx_Path/dbpsk_Tx_path_UAV.py
+++ src/Tx_Path/dbpsk_Tx_path_UAV.py
@@ -4,7 +4,7 @@
 # Title: dbpsk_Tx_path_UAV
 # Author: UAV Group 4/21/2009
 # Description: gnuradio flow graph
-# Generated: Tue Apr 21 11:17:13 2009
+# Generated: Tue May 19 16:42:39 2009
 ##################################################
 
 from gnuradio import blks2
@@ -20,8 +20,22 @@ class dbpsk_Tx_path_UAV(gr.top_block):
 		##################################################
 		# Variables
 		##################################################
+		self.variable_chooser_0 = variable_chooser_0 = [8000, 0][0]
 		self.samp_rate = samp_rate = 32000
-		self.freq_offset = freq_offset = 0
+		self.freq_offset = freq_offset = -3100
+
+		##################################################
+		# Controls
+		##################################################
+		_variable_chooser_0_control = grc_wxgui.drop_down_control(
+			window=self.GetWin(),
+			callback=self.set_variable_chooser_0,
+			label='variable_chooser_0',
+			index=0,
+			choices=[8000, 0],
+			labels=[],
+		)
+		self.Add(_variable_chooser_0_control)
 
 		##################################################
 		# Blocks
@@ -31,42 +45,45 @@ class dbpsk_Tx_path_UAV(gr.top_block):
 			excess_bw=0.35,
 			gray_code=True,
 		)
-		self.blks2_packet_encoder_0 = grc_blks2.packet_encoder(
-			item_size_in=gr.sizeof_char*1,
-			samples_per_symbol=8,
-			bits_per_symbol=1,
-			access_code="",
-			pad_for_usrp=True,
+		self.blks2_packet_encoder_0 = grc_blks2.packet_mod_b(grc_blks2.packet_encoder(
+				samples_per_symbol=8,
+				bits_per_symbol=1,
+				access_code="",
+				pad_for_usrp=False,
+			),
 			payload_length=1024,
 		)
-		self.gr_file_source_0 = gr.file_source(gr.sizeof_char*1, "insert file name here", False)
+		self.gr_file_source_0 = gr.file_source(gr.sizeof_char*1, "/home/sab/softwaredefinedradio/src/Tx_Path/Command", True)
+		self.gr_multiply_const_vxx_0 = gr.multiply_const_vcc((variable_chooser_0, ))
 		self.gr_throttle_0 = gr.throttle(gr.sizeof_gr_complex*1, (8*samp_rate))
 		self.usrp_simple_sink_x_0 = grc_usrp.simple_sink_c(which=0, side='A')
 		self.usrp_simple_sink_x_0.set_interp_rate(500)
-		self.usrp_simple_sink_x_0.set_frequency((440e6 + freq_offset), verbose=True)
+		self.usrp_simple_sink_x_0.set_frequency(439997900, verbose=True)
 		self.usrp_simple_sink_x_0.set_gain(-1)
 		self.usrp_simple_sink_x_0.set_enable(True)
 
 		##################################################
 		# Connections
 		##################################################
-		self.connect((self.gr_throttle_0, 0), (self.usrp_simple_sink_x_0, 0))
 		self.connect((self.blks2_dxpsk_mod_0, 0), (self.gr_throttle_0, 0))
 		self.connect((self.blks2_packet_encoder_0, 0), (self.blks2_dxpsk_mod_0, 0))
 		self.connect((self.gr_file_source_0, 0), (self.blks2_packet_encoder_0, 0))
+		self.connect((self.gr_throttle_0, 0), (self.gr_multiply_const_vxx_0, 0))
+		self.connect((self.gr_multiply_const_vxx_0, 0), (self.usrp_simple_sink_x_0, 0))
+
+	def set_variable_chooser_0(self, variable_chooser_0):
+		self.variable_chooser_0 = variable_chooser_0
+		self.gr_multiply_const_vxx_0.set_k((self.variable_chooser_0, ))
 
 	def set_samp_rate(self, samp_rate):
 		self.samp_rate = samp_rate
 
 	def set_freq_offset(self, freq_offset):
 		self.freq_offset = freq_offset
-		self.usrp_simple_sink_x_0.set_frequency((440e6 + self.freq_offset))
 
 if __name__ == '__main__':
 	tb = dbpsk_Tx_path_UAV()
 	tb.start()
-	#if buffer in USRP is empty, stop
-	while(buffer not empty):
-		pass
+	raw_input('Press Enter to quit: ')
 	tb.stop()
 
--- src/link/Tx_Kill/Tx_Kill.py
+++ src/link/Tx_Kill/Tx_Kill.py
@@ -4,22 +4,45 @@
 # Title: Tx_Kill
 # Author: Charles Judah
 # Description: gnuradio flow graph
-# Generated: Tue May 19 10:56:37 2009
+# Generated: Tue May 19 15:00:04 2009
 ##################################################
 
 from gnuradio import gr
 from grc_gnuradio import usrp as grc_usrp
+from grc_gnuradio import wxgui as grc_wxgui
+import wx
 
-class Tx_Kill(gr.top_block):
+class Tx_Kill(grc_wxgui.top_block_gui):
 
 	def __init__(self):
-		gr.top_block.__init__(self, "Tx_Kill")
+		grc_wxgui.top_block_gui.__init__(
+			self,
+			title="GRC - Executing: Tx_Kill",
+		)
+
+		##################################################
+		# Variables
+		##################################################
+		self.variable_chooser_0 = variable_chooser_0 = [1, 0][0]
+
+		##################################################
+		# Controls
+		##################################################
+		_variable_chooser_0_control = grc_wxgui.radio_buttons_horizontal_control(
+			window=self.GetWin(),
+			callback=self.set_variable_chooser_0,
+			label="Tx_Stop",
+			index=0,
+			choices=[1, 0],
+			labels=[],
+		)
+		self.Add(_variable_chooser_0_control)
 
 		##################################################
 		# Blocks
 		##################################################
 		self.const_source_x_0 = gr.sig_source_c(0, gr.GR_CONST_WAVE, 0, 0, 1)
-		self.gr_multiply_const_vxx_0 = gr.multiply_const_vcc((0, ))
+		self.gr_multiply_const_vxx_0 = gr.multiply_const_vcc((variable_chooser_0, ))
 		self.gr_throttle_0 = gr.throttle(gr.sizeof_gr_complex*1, 256e3)
 		self.usrp_simple_sink_x_0 = grc_usrp.simple_sink_c(which=0, side='A')
 		self.usrp_simple_sink_x_0.set_interp_rate(500)
@@ -34,9 +57,11 @@ class Tx_Kill(gr.top_block):
 		self.connect((self.gr_throttle_0, 0), (self.gr_multiply_const_vxx_0, 0))
 		self.connect((self.const_source_x_0, 0), (self.gr_throttle_0, 0))
 
+	def set_variable_chooser_0(self, variable_chooser_0):
+		self.variable_chooser_0 = variable_chooser_0
+		self.gr_multiply_const_vxx_0.set_k((self.variable_chooser_0, ))
+
 if __name__ == '__main__':
 	tb = Tx_Kill()
-	tb.start()
-	raw_input('Press Enter to quit: ')
-	tb.stop()
+	tb.Run()
 
