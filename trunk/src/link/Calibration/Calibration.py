99dce24c0dc80c428c06a2c0b51118660605d637
c.judah.75 <c.judah.75@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-05-19 23:57:57 +0000

    Tx Path added Variable Chooser to stop Transmit

--- trunk/src/link/Calibration/Calibration.py
+++ trunk/src/link/Calibration/Calibration.py
@@ -4,13 +4,14 @@
 # Title: Calibration
 # Author: Charles
 # Description: gnuradio flow graph
-# Generated: Mon May 18 12:18:40 2009
+# Generated: Tue May 19 16:40:03 2009
 ##################################################
 
 from gnuradio import blks2
 from gnuradio import gr
 from grc_gnuradio import usrp as grc_usrp
 from grc_gnuradio import wxgui as grc_wxgui
+import numpy
 import wx
 
 class Calibration(grc_wxgui.top_block_gui):
@@ -24,12 +25,23 @@ class Calibration(grc_wxgui.top_block_gui):
 		##################################################
 		# Variables
 		##################################################
+		self.variable_chooser_0 = variable_chooser_0 = [8000, 0][0]
+		self.variable_0 = variable_0 = 0
 		self.samp_rate = samp_rate = 32000
 		self.freq_offset = freq_offset = 0
 
 		##################################################
 		# Controls
 		##################################################
+		_variable_chooser_0_control = grc_wxgui.radio_buttons_horizontal_control(
+			window=self.GetWin(),
+			callback=self.set_variable_chooser_0,
+			label='variable_chooser_0',
+			index=0,
+			choices=[8000, 0],
+			labels=[],
+		)
+		self.Add(_variable_chooser_0_control)
 		_freq_offset_control = grc_wxgui.slider_horizontal_control(
 			window=self.GetWin(),
 			callback=self.set_freq_offset,
@@ -50,10 +62,10 @@ class Calibration(grc_wxgui.top_block_gui):
 			excess_bw=0.35,
 			gray_code=False,
 		)
-		self.gr_multiply_const_vxx_1 = gr.multiply_const_vcc((7000, ))
+		self.gr_multiply_const_vxx_1 = gr.multiply_const_vcc((variable_chooser_0, ))
 		self.gr_throttle_0 = gr.throttle(gr.sizeof_gr_complex*1, 256e3)
-		self.gr_vector_source_x_0 = gr.vector_source_b((0, 0, 0, 1, 1, 0, 1, 1), True)
-		self.usrp_simple_sink_x_1 = grc_usrp.simple_sink_c(which=0, side='B')
+		self.random_source_x_0 = gr.vector_source_b(numpy.random.randint(0, 1, 1000), True)
+		self.usrp_simple_sink_x_1 = grc_usrp.simple_sink_c(which=0, side='A')
 		self.usrp_simple_sink_x_1.set_interp_rate(500)
 		self.usrp_simple_sink_x_1.set_frequency((440e6+freq_offset), verbose=True)
 		self.usrp_simple_sink_x_1.set_gain(0)
@@ -65,7 +77,14 @@ class Calibration(grc_wxgui.top_block_gui):
 		self.connect((self.gr_throttle_0, 0), (self.gr_multiply_const_vxx_1, 0))
 		self.connect((self.gr_multiply_const_vxx_1, 0), (self.usrp_simple_sink_x_1, 0))
 		self.connect((self.blks2_dxpsk_mod_0, 0), (self.gr_throttle_0, 0))
-		self.connect((self.gr_vector_source_x_0, 0), (self.blks2_dxpsk_mod_0, 0))
+		self.connect((self.random_source_x_0, 0), (self.blks2_dxpsk_mod_0, 0))
+
+	def set_variable_chooser_0(self, variable_chooser_0):
+		self.variable_chooser_0 = variable_chooser_0
+		self.gr_multiply_const_vxx_1.set_k((self.variable_chooser_0, ))
+
+	def set_variable_0(self, variable_0):
+		self.variable_0 = variable_0
 
 	def set_samp_rate(self, samp_rate):
 		self.samp_rate = samp_rate
