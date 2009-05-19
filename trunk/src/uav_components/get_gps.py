c89b6d2b48251b142172a2a79dcddb2334af0432
derrickjones323@gmail.com <derrickjones323@gmail.com@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-05-19 22:16:00 +0000

    Added get_gps script, which queries gpsd for location information and writes it to a file.

---
+++ trunk/src/uav_components/get_gps.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+'''
+to be called from the UAV controller. When this script is called, it saves gps 
+location information obtained from gpsd into a text file in the current 
+directory of the script.
+'''
+##############################################################################
+import os, telnetlib, time
+HOST='localhost'
+PORT='2947'							#2947 is the port for gpsd
+gps_file = open('gps.dat','w') 		#(intentionally) overwrite file
+tn = telnetlib.Telnet(HOST,PORT)
+
+tn.write("o\n")
+gps_file.write(tn.read_some()+'\n')	#save the information returned from gpsd
+gps_file.close()
