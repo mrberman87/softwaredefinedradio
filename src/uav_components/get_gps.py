6e62080e8c3d45119072461bb3816bcb60a60294
derrickjones323@gmail.com <derrickjones323@gmail.com@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-05-20 03:05:09 +0000

    added another gps logfile for testing. deleted custom nmea parser that will no longer be used on ground station. get_gps script asks for position, speed over ground, velocity, and error instead of "everything it can get"

--- src/uav_components/get_gps.py
+++ src/uav_components/get_gps.py
@@ -11,8 +11,11 @@ HOST='localhost'
 PORT='2947'							#2947 is the port for gpsd
 gps_file = open('gps.dat','w') 		#(intentionally) overwrite file
 tn = telnetlib.Telnet(HOST,PORT)
-
-tn.write("o\n")
-gps_file.write(tn.read_some()+'\n')	#save the information returned from gpsd
+tn.write("k")
+tn.read_until('\n')
+#request gps data. see gpsd man pages for details. string is a set of single
+#letter commnands.
+tn.write("pave\n")
+gps_file.write(tn.read_until('\n'))	#save the information returned from gpsd
 gps_file.close()
 tn.close()
