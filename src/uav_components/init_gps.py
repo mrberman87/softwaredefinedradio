15a8f2f73213582cff142ec7c1cbfc074d817e6e
derrickjones323@gmail.com <derrickjones323@gmail.com@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-05-20 02:18:58 +0000

    added init_gps script, which starts gpsd to read a GPS device connected to a USB port. No error checking yet.

---
+++ src/uav_components/init_gps.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+##############################################################################
+#initializes gpsd to read the GPS 18 module on a USB port.
+##############################################################################
+import os,sys
+command = "gpsd /dev/ttyUSB"
+dev_num = 0					#depends on which port the device is connected to
+PORT = '2947'
+#SYS_CALL_ERROR = '256'
+#SYS_CALL_SUCCESS = '0'
+
+os.system(command + str(dev_num) + " -S " + PORT)
+
+#gpsd does not start polling until it has at least one request
+#so request once, then clean up
+os.system("./get_gps.py")
+os.system("rm gps.dat")
