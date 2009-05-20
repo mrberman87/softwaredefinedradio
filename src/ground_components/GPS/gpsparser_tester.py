2afbca18b2c688c09e72131c205260cc0695681e
derrickjones323@gmail.com <derrickjones323@gmail.com@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-05-20 23:53:47 +0000

    deleted init_gps script, which is not taken care of in GPS_getter. Updated gpsparser and gpsparser_tester to work with new GPS_packet

--- src/ground_components/GPS/gpsparser_tester.py
+++ src/ground_components/GPS/gpsparser_tester.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 '''
-Derrick Jones
-UAV Software Defined Radio
-Simple test driver for GPS_GGA_packet class
 '''
 
 from gpsparser import gpsparser
 
 
-my_file=open('gps_strings.txt','r');  #open file for reading
+my_file=open('gpsd_strings.txt','r');  #open file for reading
 my_line_queue=list()    
 for line in my_file.readlines():
     my_line_queue.append(line)
@@ -21,8 +18,8 @@ my_parser=gpsparser('my_log.txt')
 for item in my_line_queue:
 	my_parser.update_packet(item)
 	my_packet=my_parser.current_packet
-	print "altitude:\t", my_packet.altitude,"m"
-	print "longitude:\t", my_packet.lon, my_packet.long_hemis
+	print "altitude:\t", my_packet.alt,"m"
+	print "longitude:\t", my_packet.lon, my_packet.lon_hemis
 	print "latitude:\t", my_packet.lat, my_packet.lat_hemis
 	print "time stamp:\t", my_packet.t_hours,":",my_packet.t_mins,":",my_packet.t_secs,"\n"
 
