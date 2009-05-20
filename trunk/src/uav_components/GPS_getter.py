1a707176dc7b9ddb9aa6b62a33baec131484e0e2
derrickjones323@gmail.com <derrickjones323@gmail.com@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-05-20 21:38:42 +0000

    added GPS_getter Class that will be instantiated by UAV_controller when it initializes all the other variables. UAV_controller will be able to call the get_gps() method to update gps data in the file gps.dat

---
+++ trunk/src/uav_components/GPS_getter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+##############################################################################
+'''
+An instance of this class opens gpsd, and can then be used to write current
+GPS data into a file. currently, assumes gpsd is not running. could operate
+unexpectedly if gpsd is already running.
+'''
+##############################################################################
+import os,telnetlib, time
+
+class GPS_getter:
+	def __init__(self, HOST = 'localhost', PORT = '2947'):
+		#open gpsd to and prepare to read GPS device
+		os.system("gpsd /dev/ttyUSB0" + " -S " + PORT)
+
+		#os.system("gpsfake dl_gps.txt")
+
+		#open a telnet session to talk to gpsd
+		self.tn = telnetlib.Telnet(HOST, PORT)
+		#immediately query gpsd, because it will not bind to a GPS until
+		#it receive's its first query.
+		self.tn.write("p\n")
+		self.tn.read_until("\n")
+
+	def get_gps(self):
+		#issue the single letter queries for data to gpsd
+		self.tn.write("pave\n")
+		#read gpsd's response to queries
+		loc_data = self.tn.read_until("\n")
+		#(intentionally) overwrite file
+		gps_file = open('gps.dat','w')
+		#save the information returned from gpsd
+		gps_file.write(loc_data)
+		gps_file.close()
+
