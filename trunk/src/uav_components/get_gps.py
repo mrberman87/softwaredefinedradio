481558160ee779049ce79728ce78eaed6d6e30c7
derrickjones323@gmail.com <derrickjones323@gmail.com@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-05-20 02:18:58 +0000

    added init_gps script, which starts gpsd to read a GPS device connected to a USB port. No error checking yet.

--- trunk/src/uav_components/get_gps.py
+++ trunk/src/uav_components/get_gps.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+##############################################################################
 '''
 to be called from the UAV controller. When this script is called, it saves gps 
 location information obtained from gpsd into a text file in the current 
 directory of the script.
 '''
 ##############################################################################
-import os, telnetlib, time
+import os, telnetlib
 HOST='localhost'
 PORT='2947'							#2947 is the port for gpsd
 gps_file = open('gps.dat','w') 		#(intentionally) overwrite file
@@ -14,3 +15,4 @@ tn = telnetlib.Telnet(HOST,PORT)
 tn.write("o\n")
 gps_file.write(tn.read_some()+'\n')	#save the information returned from gpsd
 gps_file.close()
+tn.close()
