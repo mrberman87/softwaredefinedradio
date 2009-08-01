1a62f83f121dfb10325d10bd2e2842d8215901d5
mrberman87@gmail.com <mrberman87@gmail.com@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-08-01 05:37:28 +0000

    restructuring the uav controller code

---
+++ src/uav_components/controller/uav_controller.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os, time
+from payload_transmit_path import *
+
+class uav_controller:
+	def __init__(self):
+		#set priority
+		os.system("renice 0 %d" % os.getpid())
+		self.log("Starting Up: pid = %d" % os.getpid())
+		self.init_vars()
+		
+		#This is the main controller section of code
+		while True:
+			
+
+	def init_vars(self):
+		#initialize all class level variables
+		
+
+	def init_rx_msgq(self):
+		
+
+	def init_tx_msgq(self):
+		
+
+
