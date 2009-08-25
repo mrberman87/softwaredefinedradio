9c521b3a01d88ed06253040d0f824419077750de
mrberman87@gmail.com <mrberman87@gmail.com@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-08-25 03:40:49 +0000

    fixed a pebkac error

--- trunk/src/misc_files/parent.py
+++ trunk/src/misc_files/parent.py
@@ -6,12 +6,11 @@ if __name__ == '__main__':
 	pipe = subprocess.PIPE
 	p = subprocess.Popen(['python', './child.py'], shell=False, stdin=pipe, stdout=pipe, stderr=pipe, close_fds=True)
 	time.sleep(1)
-	#p.stdin.write("hello\n")
-	print p.stdout.read()
-"""	test = True
+	p.stdin.write("hello\n")
+	test = True
 	while test:
 		tmp = p.stdout.read()
 		if tmp != '':
 			test = False
 	print tmp
-"""
+
