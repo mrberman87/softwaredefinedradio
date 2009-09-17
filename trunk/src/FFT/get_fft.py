e377f5b7be1bf378df953f396828bc233d12fe88
c.judah.75 <c.judah.75@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-09-17 02:06:10 +0000

    updating fixed get_fft.py and including weird fft image

--- trunk/src/FFT/get_fft.py
+++ trunk/src/FFT/get_fft.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-import subprocess
+import subprocess, os
 
 if __name__ == '__main__':
+	os.system("rm -f /home/sab/Desktop/RC.dat")
+	os.system("touch /home/sab/Desktop/RC.dat")
 	os.spawnl(os.P_WAIT, '/usr/bin/python', 'python', 'FFT_data_aq.py')
 	subprocess.Popen("octave", stdout=subprocess.PIPE, stdin=subprocess.PIPE).communicate('UAV_fft2\n')
 
