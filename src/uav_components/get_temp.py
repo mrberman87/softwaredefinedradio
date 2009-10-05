d427a656dad2aa6048e4e55589d6a4b72c2d6c72
mrberman87@gmail.com <mrberman87@gmail.com@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-10-05 23:00:58 +0000

    Updated Telemetry.py for working circuit.

--- src/uav_components/get_temp.py
+++ src/uav_components/get_temp.py
@@ -16,10 +16,11 @@ def push_low(s):
 	time.sleep(.001)
 	s.setRTS(0)
 
-def decode_data(value):
-	out = -60.732*value*value*value + 345.5*value*value - 836.16*value + 380
+def decode_data(value1):
+	#out = -60.732*value*value*value + 345.5*value*value - 836.16*value + 380
+	out=549.69*value1*value1 - 1194.4*value1 + 439.13		
 	print "%d degrees" % out
-
+	print value1
 if __name__ == '__main__':
 	t=timeit.Timer('test()', "from __main__ import test")
 	while(True):
