3329b90a3e470e421f2da9b275ca3c0482ab9126
mrberman87@gmail.com <mrberman87@gmail.com@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-10-08 03:21:47 +0000

    making sure i have everything up to date

--- trunk/src/uav_components/controller/install.sh
+++ trunk/src/uav_components/controller/install.sh
@@ -15,12 +15,13 @@ cp ~/softwaredefinedradio/src/uav_components/controller/wd_reset.py /uav/wd_rese
 cp ~/softwaredefinedradio/src/uav_components/get_batt.py /uav/get_batt.py
 cp ~/softwaredefinedradio/src/uav_components/get_temp.py /uav/get_temp.py
 cp ~/softwaredefinedradio/src/uav_components/GPS_getter.py /uav/GPS_getter.py
-cp ~/softwaredefinedradio/src/Data_Path/packetizer.py /uav/packetizer.py
-cp ~/softwaredefinedradio/src/Data_Path/tx_rx_path.py /uav/tx_rx_path.py
-cp ~/softwaredefinedradio/src/Data_Path/txrx_controller_v2.py /uav/txrx_controller.py
+#cp ~/softwaredefinedradio/src/Data_Path/packetizer.py /uav/packetizer.py
+#cp ~/softwaredefinedradio/src/Data_Path/tx_rx_path.py /uav/tx_rx_path.py
+#cp ~/softwaredefinedradio/src/Data_Path/txrx_controller_v2.py /uav/txrx_controller.py
+cp ~/softwaredefinedradio/src/Data_Path/*.py /uav/*
 cp ~/softwaredefinedradio/src/FFT/FFT_data_aq.py /uav/FFT_data_aq.py
 cp ~/softwaredefinedradio/src/FFT/get_fft.py /uav/get_fft.py
 cp ~/softwaredefinedradio/src/FFT/UAV_fft2.m /uav/UAV_fft2.m
-mv /etc/rc.local /etc/rc.local.old
-cp ~/softwaredefinedradio/src/uav_components/rc.local.txt /etc/rc.local
+#mv /etc/rc.local /etc/rc.local.old
+#cp ~/softwaredefinedradio/src/uav_components/rc.local.txt /etc/rc.local
 chmod -R 'a'+rw /uav/*
