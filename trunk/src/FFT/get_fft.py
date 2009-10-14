0dfa0c8e653515459a031c4559ddb57922a5a7f3
mrberman87@gmail.com <mrberman87@gmail.com@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-10-14 23:49:22 +0000

    got the FFT code to dynamically take data and send the picture file in the file system

--- trunk/src/FFT/get_fft.py
+++ trunk/src/FFT/get_fft.py
@@ -5,11 +5,12 @@ import subprocess, os, sys, time
 if __name__ == '__main__':
 	to_path = sys.argv[1]
 	from_path = sys.argv[2]
-	p = subprocess.Popen('> %s' % path, shell=True)
+	p = subprocess.Popen('> %s' % from_path, shell=True)
 	p.wait()
-	p = subprocess.Popen('python FFT_data_aq.py', shell=True)
+	p = subprocess.Popen('python FFT_data_aq.py %s' % from_path, shell=True)
 	os.waitpid(p.pid, 0)
 	p = subprocess.Popen("octave", stdin=subprocess.PIPE, shell=True)
-	os.write(p.stdin.fileno(), 'UAV_fft2 %s %s\n' % (to_path, from_path))
+	os.write(p.stdin.fileno(), 'UAV_fft2(\'%s\',\'%s\')\n' % (from_path, to_path))
 	time.sleep(5)
 	os.write(p.stdin.fileno(), 'exit\n')
+
