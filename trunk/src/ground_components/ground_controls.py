87133b58aa5441bfd19019fa7152d768e98f6111
mrberman87@gmail.com <mrberman87@gmail.com@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-10-20 00:17:52 +0000

    working on the ground controller

--- trunk/src/ground_components/ground_controls.py
+++ trunk/src/ground_components/ground_controls.py
@@ -24,16 +24,18 @@ class ground_controls(abstractmodel.AbstractModel, threading.Thread):
 		self.pendingRequest = threading.Event()
 		self.cmd_qLock = threading.Lock()
 		self.gps = GPS_packet("GPSD,P=0 0,A=0,V=0,E=0")
+		self.go_home() #initialize the "GO HOME" type variables
 		self.temperature = '0'
 		self.batt = '0'
-		self.freq = '0'
-		self.modulation = 'BPSK'
-		self.timeout = '10' #(in seconds)
 		self.sigPower = '0'
 		self.cmd_list = []
 		self.MAX_COMMANDS=3
 		self.imageFileName = ''
 		self.tsvr = dummyTransmitter()
+		self.fname = ''
+		self.new_freq = 0
+		self.new_modulation = ''
+		self.new_timeout = 0
 	
 	
 	"""GUI responder methods: These are the only methods that should be
@@ -82,16 +84,16 @@ class ground_controls(abstractmodel.AbstractModel, threading.Thread):
 			cmd = self.getNextCommand()
 
 			#figure out which command has been requested
-			if cmd.startswith('GPS'):
-				self.getGPS()
-			elif cmd.startswith('Image'):
-				self.getImage()
-			elif cmd.startswith('FFT'): pass
-			elif cmd.startswith('Batt'): pass
-			elif cmd.startswith('Temp'): pass
-			elif cmd.startswith('Freq'):
-				freq = cmd.split(' ')[1] #string after the space is the freq.
-				self.changeFrequency(freq)
+			if cmd.startswith('GPS'): self.send_data('GPS')
+			elif cmd.startswith('Image'):self.send_data('Image')
+			elif cmd.startswith('FFT'): self.send_data('FFT')
+			elif cmd.startswith('Batt'): self.send_data('Telemetry')
+			elif cmd.startswith('Settings'):
+				tmp  = cmd.split(' ')
+				self.new_freq = int(tmp[1])
+				self.new_modulation = tmp[2]
+				self.new_timeout = int(tmp[3])
+				#self.new_handshake = int(tmp[4])
 			else: pass
 			
 			self.removeCompletedCommand()
@@ -103,36 +105,76 @@ class ground_controls(abstractmodel.AbstractModel, threading.Thread):
 	"""Worker methods: These methods send and recieve data from the
 	transeiver. They are I/O bound, which is why it is necessary to run them
 	in a separate thread to keep the application responsive."""
-
-	def getImage(self):
-		self.tsvr.send(data = 'Image')
-		filename = self.tsvr.receive()
-		#self.update()
 	
+	def send_data(self, data):
+		rx = True
 
-	def getGPS(self):
-		self.tsvr.send(data = 'GPS')
-		#recieve returns the name of the file it recieved
-		fname = self.tsvr.receive()
-		f = open(fname)
-		self.gps = GPS_packet(f.readline())
-		#self.update()
+		if data == 'GPS': self.fname = 'gps.dat'
+		elif data == 'Image': self.fname = 'picture.jpg'
+		elif data == 'FFT': self.fname = 'fft.jpeg'
+		elif data == 'Telemetry': self.fname = 'tele.dat'
+		elif data == 'Settings': self.fname = 'settings.dat'
 
-	def getBatt(self):
-		self.tsvr.send(data = 'sensors')
-		fname =self.tsvr.receive()
-		f = open(fname)
-		self.batt = f.readline()
-		#self.update()
+		if data == 'Settings':
+			#print things into a file to be sent up and change data to the file path
+			rx = False
+			fd = open(self.fname)
+			fd.write("settings\n")
+			if self.new_freq != self.freq: fd.write("Freq: " + self.new_freq + '\n')
+			if self.new_timeout != self.timeout: fd.write("Timeout: " + self.new_timeout + '\n')
+			#if self.new_handshake != self.handshake: fd.write("Hand_Max: " + self.new_handshake + '\n')
+			if self.new_modulation != self.modulation: fd.write("Version: " + self.new_modulation + '\n')
+			fd.close()
+		
+		tmp = self.tsvr.transmit(data)
+		if tmp is True or tmp == 'Transmission Complete':
+			self.go_home = 0
+			#decode the data sent back down
+			if data == 'GPS':
+				f = open(self.fname)
+				self.gps = GPS_packet(f.readline())
+				f.close()
+			elif data == 'Telemetry':
+				f = open(self.fname)
+				self.temprature = int(f.readline().strip('\n').strip())
+				self.batt = int(f.readline().strip('\n').strip())
+				f.close()
+			elif data == 'Settings':
+				self.freq = self.new_freq
+				self.modulation = self.new_modulation
+				self.timeout = self.new_timeout
+				#self.handshake = self.new_handshake
+		elif tmp == 'Handshaking Maximum Reached':
+			self.go_home = self.go_home + 1
+			
+		elif tmp == 'Timeout':
+			self.go_home = self.go_home + 1
+			
+		elif tmp == 'Error':
+			self.go_home = self.go_home + 1
+			
+		
+		if self.go_home >= 3:
+			#Going Home
+			rx = False
+			self.go_home()
+		
+		if rx:
+			tmp = self.tsvr.receive()
+			if tmp is True or tmp == 'Transmission Complete':
+				self.go_home = 0
+			elif tmp == 'Handshaking Maximum Reached':
+				
+			elif tmp == 'Timeout':
+				
+			elif tmp == 'Error':
+				
 
-	def changeModScheme(self, mod):
-		self.modulation = mod
-		#self.update()
-
-	def changeFrequency(self, newFreq):
-		'''will need to send command to UAV, wait for confirmation,
-		then save the new frequency in the model.'''
-		self.freq = newFreq
-		#self.update()
+	def go_home(self):
+		self.go_home = 0
+		self.freq = '0'
+		self.modulation = 'BPSK'
+		self.timeout = '10' #(in seconds)
+		#self.handshake = 5
 
 class QueueLimitException(Exception):pass
--- trunk/src/uav_components/controller/install.sh
+++ trunk/src/uav_components/controller/install.sh
@@ -14,10 +14,11 @@ cp ~/softwaredefinedradio/src/uav_components/controller/watch_dog2.py /uav/watch
 cp ~/softwaredefinedradio/src/uav_components/controller/wd_reset.py /uav/wd_reset.py
 cp ~/softwaredefinedradio/src/uav_components/telemetry.py /uav/telemetry.py
 cp ~/softwaredefinedradio/src/uav_components/GPS_getter.py /uav/GPS_getter.py
-#cp ~/softwaredefinedradio/src/Data_Path/packetizer.py /uav/packetizer.py
-#cp ~/softwaredefinedradio/src/Data_Path/tx_rx_path.py /uav/tx_rx_path.py
-#cp ~/softwaredefinedradio/src/Data_Path/txrx_controller_v2.py /uav/txrx_controller.py
-cp ~/softwaredefinedradio/src/Data_Path/* /uav/
+cp ~/softwaredefinedradio/src/Data_Path/packetizer.py /uav/packetizer.py
+cp ~/softwaredefinedradio/src/Data_Path/txrx_d8psk.py /uav/txrx_d8psk.py
+cp ~/softwaredefinedradio/src/Data_Path/txrx_dqpsk.py /uav/txrx_dqpsk.py
+cp ~/softwaredefinedradio/src/Data_Path/txrx_dbpsk.py /uav/txrxdbpsk_.py
+cp ~/softwaredefinedradio/src/Data_Path/txrx_controller.py /uav/txrx_controller.py
 cp ~/softwaredefinedradio/src/FFT/FFT_data_aq.py /uav/FFT_data_aq.py
 cp ~/softwaredefinedradio/src/FFT/get_fft.py /uav/get_fft.py
 cp ~/softwaredefinedradio/src/FFT/UAV_fft2.m /uav/UAV_fft2.m
