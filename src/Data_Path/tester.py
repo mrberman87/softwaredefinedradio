735ab1247d5d33bfc383ad4acb3ae3d8d64df6c5
mrberman87@gmail.com <mrberman87@gmail.com@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-10-29 03:36:17 +0000

    Changed the tester.py to reflect only code for testing the transceiver and not other past test codes.

--- src/Data_Path/tester.py
+++ src/Data_Path/tester.py
@@ -1,48 +1,10 @@
 #!/usr/bin/env python
 
-from txrx_controller_v2 import txrx_controller
+from txrx_controller import txrx_controller
 import time
 
-tb = txrx_controller(version = 'qpsk')
+tb = txrx_controller(fc=440e6, centoff=0, foffset_tx=0, foffset_rx=0)
 time.sleep(2)
-temp = tb.transmit('/data.txt')
-#temp = tb.receive()
+#temp = tb.transmit('/data.txt')
+temp = tb.receive()
 print temp
-
-"""
-temp = True
-commands = ['picture', 'FFT', 'telemetry', 'battery', 'temperature']
-index = 0
-counter = 0
-while temp:
-	temp = tb.transmit(commands[index])
-	index += 1
-	if index == 5:
-		index = 0
-	counter += 1
-	if counter == 10:
-		temp = False
-"""
-
-"""
-def main():
-	data = raw_input("Asking for input: ")
-	if data == '1':
-		return True
-	else:
-		data2 = data
-"""
-"""
-def spc(data):
-	payload_count = []
-	payload = ''
-	loc = 1
-	for ch in data:
-		if ch == ':':
-			payload = data[loc:]
-			break
-		else:
-			payload_count.append(ch)
-		loc += 1
-	print "Payload Count: ", ''.join(payload_count), "Payload: ", payload
-"""
