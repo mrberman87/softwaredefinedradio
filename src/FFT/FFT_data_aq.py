bd304d4b5602e2228ab39a4ce2fc646bc4ce08b3
brian.namaste <brian.namaste@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-11-11 23:10:49 +0000

    changing FFT_data_aq.py

--- src/FFT/FFT_data_aq.py
+++ src/FFT/FFT_data_aq.py
@@ -4,55 +4,53 @@
 # Title: FFT_data_aq
 # Author: UAV Group
 # Description: gnuradio flow graph
-# Generated: Wed Oct 14 20:20:39 2009
+# Generated: Wed Sep 30 19:22:42 2009
 ##################################################
 
-from gnuradio import blks2
 from gnuradio import gr
-from grc_gnuradio import wxgui as grc_wxgui
-import numpy
-import wx
+from gnuradio.eng_option import eng_option
+from grc_gnuradio import usrp as grc_usrp
+from optparse import OptionParser
+import time, sys
 
-class FFT_data_aq(grc_wxgui.top_block_gui):
+class FFT_data_aq(gr.top_block):
 
-	def __init__(self):
-		grc_wxgui.top_block_gui.__init__(
-			self,
-			title="GRC - Executing: FFT_data_aq",
-		)
+        def __init__(self):
+                gr.top_block.__init__(self, "FFT_data_aq")
+                self.path = sys.argv[1]
 
-		##################################################
-		# Variables
-		##################################################
-		self.samp_rate = samp_rate = 32000
-		self.freq_offset = freq_offset = 0
+                ##################################################
+                # Variables
+                ##################################################
+                self.samp_rate = samp_rate = 32000
+                self.freq_offset = freq_offset = 0
 
-		##################################################
-		# Blocks
-		##################################################
-		self.blks2_dxpsk_mod_0 = blks2.dbpsk_mod(
-			samples_per_symbol=8,
-			excess_bw=0.35,
-			gray_code=True,
-		)
-		self.gr_file_sink_0 = gr.file_sink(gr.sizeof_gr_complex*1, "/home/student/Documents/RC.dat")
-		self.gr_throttle_0 = gr.throttle(gr.sizeof_gr_complex*1, 256e3)
-		self.random_source_x_0 = gr.vector_source_b(numpy.random.randint(0, 1, 1024), True)
+                ##################################################
+                # Blocks
+                ##################################################
+                self.gr_file_sink_0 = gr.file_sink(gr.sizeof_gr_complex*1, self.path)
+                self.usrp_simple_source_x_0 = grc_usrp.simple_source_c(which=0, side="A", rx_ant="TX/RX")
+                self.usrp_simple_source_x_0.set_decim_rate(250)
+                self.usrp_simple_source_x_0.set_frequency((440e6 + freq_offset), verbose=True)
+                self.usrp_simple_source_x_0.set_gain(-1)
 
-		##################################################
-		# Connections
-		##################################################
-		self.connect((self.random_source_x_0, 0), (self.blks2_dxpsk_mod_0, 0))
-		self.connect((self.blks2_dxpsk_mod_0, 0), (self.gr_throttle_0, 0))
-		self.connect((self.gr_throttle_0, 0), (self.gr_file_sink_0, 0))
+                ##################################################
+                # Connections
+                ##################################################
+                self.connect((self.usrp_simple_source_x_0, 0), (self.gr_file_sink_0, 0))
 
-	def set_samp_rate(self, samp_rate):
-		self.samp_rate = samp_rate
+        def set_samp_rate(self, samp_rate):
+                self.samp_rate = samp_rate
 
-	def set_freq_offset(self, freq_offset):
-		self.freq_offset = freq_offset
+        def set_freq_offset(self, freq_offset):
+                self.freq_offset = freq_offset
+                self.usrp_simple_source_x_0.set_frequency((440e6 + self.freq_offset))
 
 if __name__ == '__main__':
-	tb = FFT_data_aq()
-	tb.Run()
+        parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+        (options, args) = parser.parse_args()
+        tb = FFT_data_aq()
+        tb.start()
+        time.sleep(2)
+        tb.stop()
 
