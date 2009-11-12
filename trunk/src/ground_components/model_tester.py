961f001889a2bdb1705b036d56ed4d47ff8b2fbd
mrberman87@gmail.com <mrberman87@gmail.com@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-11-12 02:13:19 +0000

    Ground Controls (Working: Frequency Changing, Tiemout Changing, Image, FFT, Telemetry, GPS) (Not Working: Modulation Scheme Change, Multiple Settings Change )

--- trunk/src/ground_components/model_tester.py
+++ trunk/src/ground_components/model_tester.py
@@ -7,8 +7,8 @@ import ground_controls
 m = ground_controls.ground_controls()
 m.start() #start the command processing thread
 #test list is a list of commands to add to the queue
-testlist=["GPS", "Image", "Batt", "Freq TX: 440e6+100e3 RX: 440e6-50e3"]
-
+#testlist=["GPS", "Image", "Batt", "Freq TX: 440e6+100e3 RX: 440e6-50e3"]
+testlist=["GPS"]
 for req in testlist:
 	try:
 		m.addToQueue(req)
