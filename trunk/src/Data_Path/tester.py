f27324e1d078122626c502a9309b4730eabae490
c.judah.75 <c.judah.75@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-11-13 21:53:21 +0000

    Edited some files to make the process easier and to reduce the import overhead. Files have been tested. Only changes to the transceiver path were made and not the controller itself in general. The only changes to the controller were how the scheme is chosen.

--- trunk/src/Data_Path/tester.py
+++ trunk/src/Data_Path/tester.py
@@ -3,8 +3,10 @@
 from txrx_controller import txrx_controller
 import time
 
-tb = txrx_controller(fc=440e6, centoff=11e3, foffset_tx=0, foffset_rx=50e3, frame_time_out = 45)
+tb = txrx_controller(fc=440e6, centoff=0, foffset_tx=100e3, foffset_rx=-50e3, version='qpsk')
 time.sleep(2)
-#temp = tb.transmit('Image')
-temp = tb.receive()
-print temp
+
+while raw_input('T to continue: ') == 'T':
+	temp = tb.transmit(raw_input('Message: '))
+	#temp = tb.receive()
+	print temp
