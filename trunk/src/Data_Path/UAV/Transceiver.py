91fe34b31544e68d188e8f0d9936c4f61d757822
c.judah.75 <c.judah.75@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-12-06 03:44:59 +0000

    Running for testing.

--- trunk/src/Data_Path/UAV/Transceiver.py
+++ trunk/src/Data_Path/UAV/Transceiver.py
@@ -3,46 +3,52 @@ import signal
 import time
 import txrx_controller
 
-class Transceiver:
+class Transceiver():
 	def __init__(self, controller, pid, fft_fn, mod_scheme, fft, toUAV, fromUAV):
 		try:
-			tb = txrx_controller.txrx_controller(fc=440e6, centoff=0, foffset_tx=100e3, foffset_rx=-50e3)
-			time.sleep(1.5)
+			print 'Opening txrx_controller.'
+			self.trans = txrx_controller.txrx_controller(fc=440e6, centoff=0, foffset_tx=100e3, foffset_rx=-50e3)
+			print 'Opened controller.'
 		except:
 			print 'Could not open USRP, exiting...'
-			sys.exit(0)
+			#sys.exit(0)
+		print 'Outside try block.'
+		time.sleep(1)
+		self.toUAV = toUAV
+		self.fromUAV = fromUAV
 		self.fft = fft
 		self.fft_fn = fft_fn
 		self.scheme = mod_scheme
+		print 'Right before run_trans().'
 		self.run_trans()
 
 
 	def run_trans(self):
 		print 'In definition run_trans.'
-		if fft == True:
+		if self.fft == 'True':
 			print 'Transceiver: Transmitting FFT from previous command.'
-			tb.transmit(self.fft_fn)
-			controller.fft = False
+			self.trans.transmit(self.fft_fn)
+			controller.fft = 'False'
 		while True:
-			s = os.read(fromUAV,1024)
+			s = os.read(self.fromUAV,1024)
 			s = s.split(':')
 			print str(s)
 			if s[0] == 'rx':
 				print 'Transceiver: Waiting on receive'
-				os.write(toUAV, str(tb.receive()))
+				os.write(self.toUAV, str(self.trans.receive()))
 			elif s[0] == 'close':
 				print 'Transceiver: Closing queues for shutdown.'
-				tb.close_queues()
+				self.trans.close_queues()
 			elif s[0] == 'set_frequency':
 				time.sleep(1)
 				print 'Transceiver: Changing Frequency.'
-				tb.set_frequency(s[1])
+				self.trans.set_frequency(s[1])
 			elif s[0] == 'set_timeout':
 				print 'Transceiver: Changing timeout.'
-				tb.set_frame_time_out(s[1])
+				self.trans.set_frame_time_out(s[1])
 			elif s[0] == ':':
 				pass
 			else:
 				print 'Transceiver: Transmitting.'
-				os.write(toUAV, str(tb.transmit(s[0])))
+				os.write(self.toUAV, str(self.trans.transmit(s[0])))
 				print 'Transceiver: Done Transmitting.'
--- trunk/src/Data_Path/UAV/UAV_controller_classed.py
+++ trunk/src/Data_Path/UAV/UAV_controller_classed.py
@@ -14,7 +14,7 @@ class controller():
 	def __init__(self):
 		self.fft_fn = '/fft.png'
 		self.mod_scheme = 'bpsk'
-		self.fft= False
+		self.fft= 'False'
 		self.pid = os.fork()
 		print 'pid : ', self.pid
 		self.run()
@@ -25,25 +25,22 @@ class controller():
 
 	def run(self):
 		if self.pid == 0:
-			try:
-				t = Transceiver.Transceiver(self, self.pid, self.fft_fn, self.mod_scheme, self.fft, toUAV, fromUAV)
-				while True:
-					pass
-			except:
-				print 'Unable to open USRP, closing process : ', str(self.pid)
-				os.write(toUAV, 'closing')
-				sys.exit(0)
+			#try:
+			self.t = Transceiver.Transceiver(self, self.pid, self.fft_fn, self.mod_scheme, self.fft, toUAV, fromUAV)
+			#except:
+				#print 'Unable to open USRP, closing process : ', str(self.pid)
+				#os.write(toUAV, 'closing')
+				#sys.exit(0)
 		
 		else:
-			try:
-				u = UAV.UAV(self, toTransceiver, fromTransceiver)
-				while True:
-					pass	
-			except:
-				os.write(toTransceiver, 'close:')
-				time.sleep(1)
-				os.kill(self.pid, signal.SIGTERM)
-				sys.exit(0)
+			#try:
+			self.u = UAV.UAV(self, toTransceiver, fromTransceiver)
+			#except:
+				#print 'Exception in UAV.'
+				#os.write(toTransceiver, 'close:')
+				#time.sleep(1)
+				#os.kill(self.pid, signal.SIGTERM)
+				#sys.exit(0)
 
 if __name__ == '__main__':
-	controller()
+	c = controller()
