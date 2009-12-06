ffd2c931146f556e9ec3e2c2036ee1858c31376d
c.judah.75 <c.judah.75@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-12-06 03:00:08 +0000

    I decided to act like derrick and mike and class everything in separate files to abstract it and make it more complicated.

---
+++ trunk/src/Data_Path/UAV/UAV_controller_classed.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import time
+import os
+import signal
+import UAV
+import Transceiver
+
+fromUAV,toTransceiver = os.pipe()
+fromTransceiver,toUAV = os.pipe()
+
+class controller():
+	def __init__(self):
+		self.fft_fn = '/fft.png'
+		self.mod_scheme = 'bpsk'
+		self.fft= False
+		self.pid = os.fork()
+		print 'pid : ', self.pid
+		self.run()
+
+	def forkit(self):
+		self.pid = os.fork()
+		self.run()
+
+	def run(self):
+		if self.pid == 0:
+			try:
+				t = Transceiver.Transceiver(self, self.pid, self.fft_fn, self.mod_scheme, self.fft, toUAV, fromUAV)
+			except:
+				print 'Unable to open USRP, closing process : ', str(self.pid)
+				os.write(toUAV, 'closing')
+				sys.exit(0)
+		
+		else:
+			try:
+				u = UAV.UAV(self, toTransceiver, fromTransceiver)
+			except:
+				os.write(toTransceiver, 'close:')
+				time.sleep(1)
+				os.kill(self.pid, signal.SIGTERM)
+				sys.exit(0)
+
+if __name__ == '__main__':
+	controller()
