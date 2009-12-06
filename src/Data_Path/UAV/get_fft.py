edea1281be2beecf7eab2d6f58471c424fe6b483
c.judah.75 <c.judah.75@60a8cbf6-ecae-11dd-9ba5-89a75009fd5d>
2009-12-06 05:03:36 +0000

    More fixes

--- src/Data_Path/UAV/get_fft.py
+++ src/Data_Path/UAV/get_fft.py
@@ -14,13 +14,11 @@ if __name__ == '__main__':
 			fc = sys.argv[3]
 	except: 
 		fc = str(440e6)
-	fnull = open(os.devnull, 'w')
 	p = subprocess.Popen('python FFT_data_aq.py %s %s %s' % (real_path, imag_path, fc), shell=True)
 	time.sleep(1)
 	p.wait()
-	p = subprocess.Popen("octave", stdin=subprocess.PIPE, stdout = fnull, shell=True)
+	p = subprocess.Popen("octave", stdin=subprocess.PIPE, stdout=open(os.devnull, 'w'), shell=True)
 	os.write(p.stdin.fileno(), 'UAV_fft2(\'%s\',\'%s\',\'%s\')\n' % (real_path, imag_path, to_path))
 	time.sleep(1)
 	p.wait()
-	fnull.close()
 
